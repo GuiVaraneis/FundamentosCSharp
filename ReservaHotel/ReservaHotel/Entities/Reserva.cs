db38fcbd8c4848456bf47f79dda9e11340023675
GuiVaraneis <guivaraneis@gmail.com>
2025-01-28 15:48:11 -0300

    Programa que consiste no usuário armazenar o ńúmero do quarto, a data de inicio e data final da hośpedagem

---
+++ ReservaHotel/ReservaHotel/Entities/Reserva.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservaHotel.Entities {
+    class Reserva {
+        public int NumeroDoQuarto { get; set; }
+        public DateTime CheckIn { get; set; }
+
+        public DateTime CheckOut { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int numeroDoQuarto, DateTime checkIn, DateTime checkOut) {
+            NumeroDoQuarto = numeroDoQuarto;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+
+        public int Duration() {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public override string ToString() {
+            return "Quarto: "
+                + NumeroDoQuarto
+                + ", Check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", Check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + "Nights";
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut) {
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+    }
+}
---
+++ ReservaHotel/ReservaHotel/Program.cs
@@ -0,0 +1,27 @@
+﻿using ReservaHotel.Entities;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Numero do quarto: ");
+        int numeroQuarto = int.Parse(Console.ReadLine());
+        Console.Write("Check-in(dd/mm/yyyy): ");
+        DateTime checkIn = DateTime.Parse(Console.ReadLine());
+        Console.Write("Check-out(dd/mm/yyyy): ");
+        DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+        if (checkOut <= checkIn) {
+            Console.WriteLine("Erro!!! data do Check-Out é menor que a data do Check-In");
+        }
+        else {
+            Reserva reserva = new Reserva(numeroQuarto, checkIn, checkOut);
+            Console.WriteLine("Reserva: " + reserva);
+        }
+    }
+
+
+
+
+}
+
+
+
